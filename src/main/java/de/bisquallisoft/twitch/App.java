c480fb435d388532200491a48da0b50d9e1155de
Kirill <kpovolotsk@zoho.com>
2014-07-05 20:02:29 +0200

    resolve #1

--- src/main/java/de/bisquallisoft/twitch/App.java
+++ src/main/java/de/bisquallisoft/twitch/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -26,6 +27,7 @@ public class App extends Application {
         MainController controller = loader.getController();
         controller.setPrimaryStage(primaryStage);
 
+        primaryStage.getIcons().add(new Image("/app-icon.png"));
         primaryStage.show();
     }
 
