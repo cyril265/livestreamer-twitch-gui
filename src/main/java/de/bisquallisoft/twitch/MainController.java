ab6e60fb5f86cd87eb21b090691ea3c510bf6872
Kirill <kpovolotsk@zoho.com>
2014-07-28 23:41:46 +0200

    lowered retry delay to 1s

--- src/main/java/de/bisquallisoft/twitch/MainController.java
+++ src/main/java/de/bisquallisoft/twitch/MainController.java
@@ -215,7 +215,7 @@ public class MainController implements Initializable {
 
 
         try {
-            new ProcessBuilder("livestreamer", url, settings.getQuality(), "--no-version-check", "-Q", "--retry-open", "3", "--retry-streams", "5").start();
+            new ProcessBuilder("livestreamer", url, settings.getQuality(), "--no-version-check", "-Q", "--retry-open", "3", "--retry-streams", "1").start();
             livestreamerProgess.setVisible(true);
             livestreamerProgess.setProgress(-1);
 
